1b98d28e3c416e68e70c0050fdc431fc5be5ca25
Radek Fryšták <geniv.radek@gmail.com>
2014-11-16 19:51:04 +0100

    prepis radiciho systemu
    pro akceptaci nativniho razeni

--- Goodflow/Finder.php
+++ Goodflow/Finder.php
@@ -12,40 +12,60 @@ namespace Goodflow;
  * rozsireni API finderu o moznost razeni
  * source: http://forum.nette.org/cs/5331-2010-09-15-trida-nette-finder-pro-prochazeni-adresarovou-strukturou#p46244
  *
- * @package geniv/goodglow
+ * @package goodglow
  * @author geniv
- * @version 1.0
+ * @version 1.04
  */
 class Finder extends \Nette\Utils\Finder
 {
-    private $order;
+    private $userDefinedSort;
 
     /**
-     * Sets the order comparison function
-     * @param callback $order
+     * Sets the user defined sprt comparison function
+     * @param callback $callback
      * @return Finder
      */
-    public function order($order)
+    public function userDefinedSort($callback)
     {
-        $this->order = $order;
+        $this->userDefinedSort = $callback;
         return $this;
     }
 
 
+    /**
+     * order by name
+     * @return Finder
+     */
     public function orderByName()
     {
-        $this->order = function($f1, $f2) {
+        $this->userDefinedSort = function($f1, $f2) {
             return \strcasecmp($f1->getFilename(), $f2->getFilename());
         };
         return $this;
     }
 
 
+    /**
+     * order by modify time
+     * @return Finder
+     */
     public function orderByMTime()
     {
-        $this->order = function($f1, $f2) {
+        $this->userDefinedSort = function($f1, $f2) {
             return $f2->getMTime() - $f2->getMTime();
         };
+        return $this;
+    }
+
+
+    /**
+     * order by natural name
+     * @param  boolean $caseSensitive true = case sensitive, false = case insensitive
+     * @return Finder
+     */
+    public function orderByNatularName($caseSensitive = true)
+    {
+        $this->userDefinedSort = ($caseSensitive ? 'natsort' : 'natcasesort');
         return $this;
     }
 
@@ -57,13 +77,17 @@ class Finder extends \Nette\Utils\Finder
     public function getIterator()
     {
         $iterator = parent::getIterator();
-        if ($this->order === NULL) {
+        if ($this->userDefinedSort === NULL) {
             return $iterator;
         }
 
         $iterator = new \ArrayIterator(\iterator_to_array($iterator));
-        $iterator->uasort($this->order);
-
+        if (is_string($this->userDefinedSort)) {
+            $sort = $this->userDefinedSort;
+            $iterator->$sort();
+        } else {
+            $iterator->uasort($this->userDefinedSort);
+        }
         return $iterator;
     }
 }
